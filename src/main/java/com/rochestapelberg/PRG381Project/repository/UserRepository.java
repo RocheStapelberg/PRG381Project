514336cc18e650c9523679d321c55d2963b572d3
Leon Stassen <64901703+Leon-Stassen@users.noreply.github.com>
2022-08-25 19:49:31 +0200

    Added user repository class.

---
+++ src/main/java/com/rochestapelberg/PRG381Project/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.rochestapelberg.PRG381Project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rochestapelberg.PRG381Project.Model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+    List<User> findByRoles(String roles);
+}

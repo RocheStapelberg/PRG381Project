bf9cfa7b25e61d0d398516729cb20ed5e0f062db
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-08-25 18:45:46 +0200

    Enabled Jpa Repositories

--- src/main/java/com/rochestapelberg/PRG381Project/Prg381ProjectApplication.java
+++ src/main/java/com/rochestapelberg/PRG381Project/Prg381ProjectApplication.java
@@ -2,8 +2,10 @@ package com.rochestapelberg.PRG381Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories()
 public class Prg381ProjectApplication {
 
 	public static void main(String[] args) {

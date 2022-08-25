d838d79caab6e7b7c9f0006383c9e3f0c809a52a
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-08-25 20:06:22 +0200

    Fixed imports

--- src/main/java/com/rochestapelberg/PRG381Project/controller/StudentController.java
+++ src/main/java/com/rochestapelberg/PRG381Project/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.rochestapelberg.PRG381Project.controller;
 
 import java.util.Optional;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 
-import com.rochestapelberg.PRG381Project.Model.User;
+import com.rochestapelberg.PRG381Project.model.User;
 import com.rochestapelberg.PRG381Project.auth.ApplicationUserService;
 
 @Controller
@@ -23,7 +22,7 @@ public class StudentController {
     private ApplicationUserService service;
 
     @GetMapping()
-    public ModelAndView viewHomePage(@NotNull Model model, @AuthenticationPrincipal UserDetails currentUser){
+    public ModelAndView viewHomePage(Model model, @AuthenticationPrincipal UserDetails currentUser){
         Optional<User> student = service.getStudentByUsername(currentUser.getUsername());
         ModelAndView mav = new ModelAndView("studentView");
         mav.addObject("student", student);
